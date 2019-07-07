03a7d19418d692c7ece174676600233b21e87f46
hamlethuseynov <h.hamlet93@gmail.com>
2019-07-07 22:52:20 +0400

    Datebase created with Codefirst

---
+++ Library/Library/Migrations/Configuration.cs
@@ -0,0 +1,21 @@
+﻿namespace Library.Migrations
+{
+    using Library.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Library.DAL.LibraryContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Library.DAL.LibraryContext context)
+        {
+            
+        }
+    }
+}
